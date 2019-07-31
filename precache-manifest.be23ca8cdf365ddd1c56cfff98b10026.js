4f0173a2dc27c85291e1190fffde7bb9b3cea088
RZFalcore <box.rz.box@gmail.com>
2019-07-31 15:43:55 +0300

    Updates

--- precache-manifest.be23ca8cdf365ddd1c56cfff98b10026.js
+++ precache-manifest.be23ca8cdf365ddd1c56cfff98b10026.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f7c687f8afdd33816666508fd05e16db",
+    "revision": "ac79db872889b4dbb154a60a09d9dc9c",
     "url": "/goit-react-hw-01-components/index.html"
   },
   {
-    "revision": "8a4db2d24e138beceb02",
-    "url": "/goit-react-hw-01-components/static/css/main.162a39b4.chunk.css"
+    "revision": "9932fdf35605c4e2e275",
+    "url": "/goit-react-hw-01-components/static/css/main.5abf2d9c.chunk.css"
   },
   {
     "revision": "6bd6c87b3d1f988fc396",
     "url": "/goit-react-hw-01-components/static/js/2.a871c82b.chunk.js"
   },
   {
-    "revision": "8a4db2d24e138beceb02",
-    "url": "/goit-react-hw-01-components/static/js/main.8f1d3c49.chunk.js"
+    "revision": "9932fdf35605c4e2e275",
+    "url": "/goit-react-hw-01-components/static/js/main.d46b8470.chunk.js"
   },
   {
     "revision": "b0f8f2e436fc577230d9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.659ab119563a2eda518e3ac93bf5c376.js"
+  "/goit-react-hw-01-components/precache-manifest.be23ca8cdf365ddd1c56cfff98b10026.js"
 );
 
 self.addEventListener('message', (event) => {
