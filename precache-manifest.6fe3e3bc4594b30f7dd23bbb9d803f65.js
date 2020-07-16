8d80f38da4f9a5e81464fca9985e195366ce577a
RZFalcore <box.rz.box@gmail.com>
2020-07-16 17:53:43 +0300

    Updates

---
+++ precache-manifest.6fe3e3bc4594b30f7dd23bbb9d803f65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "473793117d53c1aa43e4e0932fbab9f4",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "acb0d947a4b312c36eaa",
+    "url": "/goit-react-hw-01-components/static/css/main.37baccd3.chunk.css"
+  },
+  {
+    "revision": "8d9dc58c1a4c85358da3",
+    "url": "/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acb0d947a4b312c36eaa",
+    "url": "/goit-react-hw-01-components/static/js/main.587a2ecf.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-01-components/precache-manifest.be23ca8cdf365ddd1c56cfff98b10026.js"
+  "/goit-react-hw-01-components/precache-manifest.6fe3e3bc4594b30f7dd23bbb9d803f65.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-01-components/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
